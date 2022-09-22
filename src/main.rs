79f10f76704693c20e686a886dfb8854edd23169
Pankaj Raghav <p.raghav@samsung.com>
2022-09-22 17:52:48 +0200

    main: create chunk tree cache by walking through the chunk tree root
    Added support to walking through the chunk root tree to fill the cache
    for logical to physical mapping.
    
    The leaf condition works but with the given mkfs options, no nodes were
    created.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
-use std::env;
 use std::fs::File;
 use std::io;
+use std::{env, os::unix::prelude::FileExt};
 
+use btrfs_internals::structs::{BtrfsHeader, BtrfsItem, BtrfsKeyPtr};
 use btrfs_internals::{
     chunk_tree::{ChunkTree, ChunkTreeKey},
     structs::{BtrfsChunk, BtrfsKey, BtrfsStripe, BtrfsSuperblock, BTRFS_CHUNK_ITEM_KEY},
@@ -56,7 +57,87 @@ fn parse_sys_chunk_array(sb: &BtrfsSuperblock) -> Result<ChunkTree, i32> {
     Ok(chunk_tree)
 }
 
-// fn read_chunk_tree_root()
+fn read_chunk_tree_root(
+    file: &File,
+    chunk_logical_root: u64,
+    cache: &ChunkTree,
+) -> io::Result<Vec<u8>> {
+    let size = cache
+        .find_logical(chunk_logical_root)
+        .expect("Can't find the chunk")
+        .0
+        .size;
+
+    let mut chunk_root = vec![0; size as usize];
+
+    let physical_off = cache
+        .offset(chunk_logical_root)
+        .expect("Can't find the chunk");
+
+    let root = file.read_exact_at(&mut chunk_root, physical_off)?;
+
+    Ok(chunk_root)
+}
+
+fn walk_chunk_root_tree(
+    file: &File,
+    buf: &Vec<u8>,
+    cache: &mut ChunkTree,
+    nodesize: u32,
+) -> io::Result<()> {
+    let header = unsafe { &*(buf.as_ptr() as *const BtrfsHeader) };
+
+    // At the leaf
+    if header.level == 0 {
+        for i in 0..header.nritems as usize {
+            let item = unsafe {
+                &*((buf.as_ptr() as usize
+                    + std::mem::size_of::<BtrfsHeader>()
+                    + (i * std::mem::size_of::<BtrfsItem>()))
+                    as *const BtrfsItem)
+            };
+
+            if item.key.ty != BTRFS_CHUNK_ITEM_KEY {
+                continue;
+            }
+
+            let chunk = unsafe {
+                &*((buf.as_ptr() as usize
+                    + std::mem::size_of::<BtrfsHeader>()
+                    + item.offset as usize) as *const BtrfsChunk)
+            };
+
+            let off = item.key.offset;
+            let len = chunk.length;
+            let phy_off = chunk.stripe.offset;
+            cache
+                .insert(
+                    ChunkTreeKey {
+                        start: item.key.offset,
+                        size: chunk.length,
+                    },
+                    chunk.stripe.offset,
+                )
+                .unwrap_or_else(|_| panic!("Error inserting cache"));
+        }
+    } else {
+        println!("Node");
+        for i in 0..header.nritems as usize {
+            let keyptr = unsafe {
+                &*((buf.as_ptr() as usize
+                    + std::mem::size_of::<BtrfsHeader>()
+                    + (i * std::mem::size_of::<BtrfsKeyPtr>()))
+                    as *const BtrfsKeyPtr)
+            };
+            let physical_offset = cache.offset(keyptr.blockptr).expect("error getting offset");
+
+            let mut node = vec![0; nodesize as usize];
+            file.read_exact_at(&mut node, physical_offset)?;
+            walk_chunk_root_tree(&file, &node, cache, nodesize)?;
+        }
+    }
+    Ok(())
+}
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
@@ -69,9 +150,19 @@ fn main() -> io::Result<()> {
     let mut superblock = BtrfsSuperblock::new();
 
     superblock.check_valid_superblock(&file, false)?;
-    let parsed_chunktree = parse_sys_chunk_array(&superblock).unwrap_or_else(|error| {
+    let mut chunktree_cache = parse_sys_chunk_array(&superblock).unwrap_or_else(|error| {
         panic!("Error parsing the sys chunk arr");
     });
+
+    let chunk_tree_root = read_chunk_tree_root(&file, superblock.chunk_root, &chunktree_cache)?;
+
+    walk_chunk_root_tree(
+        &file,
+        &chunk_tree_root,
+        &mut chunktree_cache,
+        superblock.node_size,
+    )?;
+    println!("{}", chunktree_cache);
 
     Ok(())
 }
