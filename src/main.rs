9ee150d7251115a820712097c2f4b84b142d608a
Pankaj Raghav <p.raghav@samsung.com>
2022-09-26 17:09:43 +0200

    fs_tree: created a hashmap with inoderef items for easy lookup

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 #![allow(unused_variables)]
 #![allow(dead_code)]
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::os::unix::prelude::FileExt;
 
-use anyhow::{bail, Result};
+use anyhow::{bail, Ok, Result};
 use btrfs_internals::chunk_tree_cache::ChunkTree;
 use btrfs_internals::ctree::{parse_sys_chunk_array, read_chunk_tree_root, walk_chunk_root_tree};
 use btrfs_internals::structs::{
-    BtrfsHeader, BtrfsItem, BtrfsRootItem, BtrfsSuperblock, BTRFS_FS_TREE_OBJECTID,
-    BTRFS_ROOT_ITEM_KEY,
+    BtrfsHeader, BtrfsInodeRef, BtrfsItem, BtrfsKey, BtrfsKeyPtr, BtrfsRootItem, BtrfsSuperblock,
+    BTRFS_FS_TREE_OBJECTID, BTRFS_INODE_REF_KEY, BTRFS_ROOT_ITEM_KEY,
 };
 
 fn read_root_tree(file: &File, root_logical: u64, cache: &ChunkTree) -> Result<Vec<u8>> {
@@ -65,6 +66,77 @@ fn read_fs_tree_root(
     }
     Ok(buf)
 }
+
+#[derive(Debug)]
+struct InodeRefT {
+    key: BtrfsKey,
+    name: String,
+}
+
+fn read_inode_ref_items(
+    file: &File,
+    fs_tree: &Vec<u8>,
+    cache: &ChunkTree,
+    nodesize: u32,
+    inode_ref_cache: &mut HashMap<u64, InodeRefT>,
+) -> Result<()> {
+    let header = unsafe { &*(fs_tree.as_ptr() as *const BtrfsHeader) };
+
+    // At the leaf
+    if header.level == 0 {
+        for i in 0..header.nritems as usize {
+            let item = unsafe {
+                &*((fs_tree.as_ptr() as usize
+                    + std::mem::size_of::<BtrfsHeader>()
+                    + (i * std::mem::size_of::<BtrfsItem>()))
+                    as *const BtrfsItem)
+            };
+
+            if item.key.ty != BTRFS_INODE_REF_KEY {
+                continue;
+            }
+
+            let inode_ref = unsafe {
+                &*((fs_tree.as_ptr() as usize
+                    + std::mem::size_of::<BtrfsHeader>()
+                    + item.offset as usize) as *const BtrfsInodeRef)
+            };
+
+            let inode_name_slice = unsafe {
+                std::slice::from_raw_parts(
+                    (inode_ref as *const _ as *const u8).add(std::mem::size_of::<BtrfsInodeRef>()),
+                    inode_ref.name_len as usize,
+                )
+            };
+            let name = std::str::from_utf8(inode_name_slice)?.to_string();
+
+            inode_ref_cache.insert(
+                item.key.objectid,
+                InodeRefT {
+                    key: item.key,
+                    name: name,
+                },
+            );
+        }
+    } else {
+        println!("Node");
+        for i in 0..header.nritems as usize {
+            let keyptr = unsafe {
+                &*((fs_tree.as_ptr() as usize
+                    + std::mem::size_of::<BtrfsHeader>()
+                    + (i * std::mem::size_of::<BtrfsKeyPtr>()))
+                    as *const BtrfsKeyPtr)
+            };
+
+            let physical_offset = cache.offset(keyptr.blockptr).expect("error getting offset");
+
+            let mut node = vec![0; nodesize as usize];
+            file.read_exact_at(&mut node, physical_offset)?;
+            read_inode_ref_items(file, fs_tree, cache, nodesize, inode_ref_cache)?;
+        }
+    }
+    Ok(())
+}
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
@@ -96,7 +168,14 @@ fn main() -> Result<()> {
     // read root tree to find fs tree
     let root_tree = read_root_tree(&file, superblock.root, &chunktree_cache)?;
 
-    let fs_tree_root = read_fs_tree_root(&file, &root_tree, &chunktree_cache, superblock.node_size)?;
+    let fs_tree_root =
+        read_fs_tree_root(&file, &root_tree, &chunktree_cache, superblock.node_size)?;
+
+    let mut inode_ref_map = HashMap::new();
+
+    read_inode_ref_items(&file, &fs_tree_root, &chunktree_cache, superblock.node_size, &mut inode_ref_map)?;
+
+    println!("{:?}", inode_ref_map);
 
     Ok(())
 }
